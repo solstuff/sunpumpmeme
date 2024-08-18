28f74165870978d9bcff42d9cf968b7ddcab8a90
slimey <spen3765@gmail.com>
2024-08-18 20:17:09 +0100

    Create token_holders.py

---
+++ token_holders.py
@@ -0,0 +1,13 @@
+import requests
+from datetime import datetime
+
+url = "https://api.sunpump.meme/pump-api/token/holders?page=%d&size=10&address=TXL6rJbvmjD46zeN1JssfgxvSo99qC8MRT"
+
+X = 0
+for _ in range(2):
+    X += 1
+    data = requests.get(url % (X)).json()["data"]["holders"]
+    print(f"\nPage {X}")
+    for holder in data:
+        print(f"Address: {holder['address']}\nPercentage: {holder['percentage']:,.1f}%")
+        if holder["holderType"] != "NORMAL_USER": print("Who.is:", holder["holderType"])

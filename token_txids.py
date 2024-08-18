286aec2a0c9cf9bb3db0f3264bc6cd5fbb7f7f73
slimey <spen3765@gmail.com>
2024-08-18 21:17:26 +0100

    Create token_txids.py

---
+++ token_txids.py
@@ -0,0 +1,13 @@
+import requests
+from datetime import datetime
+
+url = "https://api.sunpump.meme/pump-api/transactions/token/TCR2f2EZEriqFV2b6nfnsZ7TUcVRyx8U8S?page=1&size=5&sort=volumeInUsd:DESC"
+
+
+data = requests.get(url).json()["data"]["swaps"]
+for tx in data:
+    print("Address:", tx['userAddress'])
+    print(f"Amount: ${tx['volumeInUsd']:,.2f}")
+    print("Event:", tx['txnOrderType'])
+
+print("Top 5 biggest transactions for", tx['toTokenSymbol'])
